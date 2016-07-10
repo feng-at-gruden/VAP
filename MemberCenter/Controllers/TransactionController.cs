40b8664803c197c04dd5946351584f39b2fc5d6d
Feng Guo <feng.guo@gruden.com>
2016-07-10 21:38:42 +0800

    Init project, implemented login, register, authentication

---
+++ MemberCenter/Controllers/TransactionController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MemberCenter;
+
+namespace MemberCenter.Controllers
+{
+    [Authorize]
+    public class TransactionController : Controller
+    {
+        private Model1Container db = new Model1Container();
+
+        // GET: /Transaction/
+        public ActionResult Index()
+        {
+            var cashtransactions = db.CashTransactions.Include(c => c.Member).Include(c => c.PaymentMethod);
+            return View(cashtransactions.ToList());
+        }
+
+        // GET: /Transaction/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CashTransaction cashtransaction = db.CashTransactions.Find(id);
+            if (cashtransaction == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cashtransaction);
+        }
+
+        // GET: /Transaction/Create
+        public ActionResult Create()
+        {
+            ViewBag.MemberId = new SelectList(db.Members, "Id", "Email");
+            ViewBag.Id = new SelectList(db.PaymentMethods, "Id", "Bank");
+            return View();
+        }
+
+        // POST: /Transaction/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="Id,MemberId,Amount,DateTime,Type,Status")] CashTransaction cashtransaction)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CashTransactions.Add(cashtransaction);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MemberId = new SelectList(db.Members, "Id", "Email", cashtransaction.MemberId);
+            ViewBag.Id = new SelectList(db.PaymentMethods, "Id", "Bank", cashtransaction.Id);
+            return View(cashtransaction);
+        }
+
+        // GET: /Transaction/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CashTransaction cashtransaction = db.CashTransactions.Find(id);
+            if (cashtransaction == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MemberId = new SelectList(db.Members, "Id", "Email", cashtransaction.MemberId);
+            ViewBag.Id = new SelectList(db.PaymentMethods, "Id", "Bank", cashtransaction.Id);
+            return View(cashtransaction);
+        }
+
+        // POST: /Transaction/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="Id,MemberId,Amount,DateTime,Type,Status")] CashTransaction cashtransaction)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cashtransaction).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MemberId = new SelectList(db.Members, "Id", "Email", cashtransaction.MemberId);
+            ViewBag.Id = new SelectList(db.PaymentMethods, "Id", "Bank", cashtransaction.Id);
+            return View(cashtransaction);
+        }
+
+        // GET: /Transaction/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CashTransaction cashtransaction = db.CashTransactions.Find(id);
+            if (cashtransaction == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cashtransaction);
+        }
+
+        // POST: /Transaction/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CashTransaction cashtransaction = db.CashTransactions.Find(id);
+            db.CashTransactions.Remove(cashtransaction);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- MemberCenter/Models/AccountViewModels.cs
+++ MemberCenter/Models/AccountViewModels.cs
@@ -30,14 +30,19 @@ namespace MemberCenter.Models
 
     public class LoginViewModel
     {
-        [Required]
-        [Display(Name = "User name")]
-        public string UserName { get; set; }
+        [Required(ErrorMessage = "请输入{0}")]
+        [Display(Name = "邮箱")]
+        [EmailAddress]
+        public string Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "请输入{0}")]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "密码")]
         public string Password { get; set; }
+
+        [Display(Name = "验证码")]
+        [Required(ErrorMessage = "请输入{0}")]
+        public string Captcha { get; set; }
 
         [Display(Name = "Remember me?")]
         public bool RememberMe { get; set; }
@@ -45,19 +50,32 @@ namespace MemberCenter.Models
 
     public class RegisterViewModel
     {
-        [Required]
-        [Display(Name = "User name")]
-        public string UserName { get; set; }
+        [Required(ErrorMessage="请输入{0}")]
+        [Display(Name = "邮箱")]
+        [EmailAddress]
+        public string Email { get; set; }
 
-        [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [Required(ErrorMessage = "请输入{0}")]
+        [StringLength(30, ErrorMessage = "{0}长度不足{2}.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "密码")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Display(Name = "确认密码")]
+        [Compare("Password", ErrorMessage = "两次输入密码不相符.")]
         public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "请输入{0}")]
+        [Display(Name = "推荐人")]
+        [EmailAddress]
+        public string Referral { get; set; }
+
+        [Display(Name = "验证码")]
+        [Required(ErrorMessage = "请输入{0}")]
+        public string Captcha { get; set; }
+
+        [Display(Name = "同意条款")]
+        public bool AcceptTerm { get; set; }
     }
 }
