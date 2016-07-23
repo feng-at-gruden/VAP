acbae03148dcadb4cb75503c6efa077b22c65f15
xiaosulan <xiaosu.lan@gruden.com>
2016-07-23 19:39:24 +0800

    members and news

---
+++ Backend/Controllers/MemberController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Backend.Models;
+
+namespace Backend.Controllers
+{
+    public class MemberController : Controller
+    {
+        private vapEntities1 db = new vapEntities1();
+
+        // GET: /Member/
+        public ActionResult Index()
+        {
+            var members = db.Members.Include(m => m.Member1);
+            return View(members.ToList());
+        }
+
+      
+
+
+        // GET: /Member/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Member member = db.Members.Find(id);
+            if (member == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Referral_Id = new SelectList(db.Members, "Id", "Email", member.Referral_Id);
+            return View(member);
+        }
+
+        // POST: /Member/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="Id,Email,UserName,RealName,Password1,Password2,Password3,Cash1,Cash2,Point1,Point2,ChongXiao1,ChongXiao2,Coin1,Coin2,RegisterTime,Level,Achievement,Status,TiXianStatus,TiBiStatus,IdSubmitted,IdApproved,ApprovedBy,Referral_Id")] Member member)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(member).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Referral_Id = new SelectList(db.Members, "Id", "Email", member.Referral_Id);
+            return View(member);
+        }
+
+        // GET: /Member/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Member member = db.Members.Find(id);
+            if (member == null)
+            {
+                return HttpNotFound();
+            }
+            return View(member);
+        }
+
+        // POST: /Member/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Member member = db.Members.Find(id);
+            db.Members.Remove(member);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
