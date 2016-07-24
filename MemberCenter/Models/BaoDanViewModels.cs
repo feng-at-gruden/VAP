59cfe52ed98f501b39ae7bd4f3ec909345161a2d
Feng Guo <feng.guo@gruden.com>
2016-07-24 13:17:40 +0800

    implemented my baodan history

--- MemberCenter/Models/BaoDanViewModels.cs
+++ MemberCenter/Models/BaoDanViewModels.cs
@@ -18,11 +18,14 @@ namespace MemberCenter.Models
         [Display(Name = "报单个数")]
         public decimal RequestQuantity { get; set; }
 
-        [Display(Name = "消耗现金")]
-        public decimal CostCash { get; set; }
-
         [Display(Name = "报单总额")]
-        public decimal TotalCash { get; set; }
+        public decimal RequestCash { get; set; }
+
+        [Display(Name = "手续费")]
+        public decimal Fee { get; set; }
+
+        [Display(Name = "消耗现金")]
+        public decimal TotalCostCash { get; set; }
 
         [Display(Name = "余额")]
         public decimal CashLeft { get; set; }
@@ -31,21 +34,63 @@ namespace MemberCenter.Models
     public class LockedCoinsViewModel
     {
         public Int32 BaoDanId { get; set; }
+
         public DateTime BaoDanTime { get; set; }
-        [DisplayFormat(DataFormatString = "{0:n0}")]
+
+        [DisplayFormat(DataFormatString = "{0:n8}")]
         public decimal BaoDanQuantity { get; set; }
-        [DisplayFormat(DataFormatString = "{0:n0}")]
+
+        [DisplayFormat(DataFormatString = "{0:n2}")]
         public decimal BaoDanPrice { get; set; }
-        [DisplayFormat(DataFormatString = "{0:0.00}")]
+
+        [DisplayFormat(DataFormatString = "{0:n2}")]
         public decimal LastPrice { get; set; }
-        [DisplayFormat(DataFormatString = "{0:0.00}")]
+
+        [DisplayFormat(DataFormatString = "{0:n2}")]
         public decimal NextPrice { get; set; }
-        [DisplayFormat(DataFormatString = "{0:0.00}")]
+
+        [DisplayFormat(DataFormatString = "{0:n8}")]
         public decimal LockedAmount { get; set; }
-        [DisplayFormat(DataFormatString = "{0:#,##0.00}")]
+
+        [DisplayFormat(DataFormatString = "{0:n8}")]
         public decimal AvailableAmount { get; set; }
-        [DisplayFormat(DataFormatString = "{0:0.00}")]
+
+        [DisplayFormat(DataFormatString = "{0:n8}")]
         public decimal AmountLeft { get; set; }
+
         public String Status { get; set; }
     }
+
+    public class BaoDanHistoryViewModel
+    {
+        public DateTime BaoDanTime { get; set; }
+
+        public String Status { get; set; }
+
+        public String Type { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:n8}")]
+        public decimal RequestQuantity { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:n2}")]
+        public decimal RequestAmount { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:n2}")]
+        public decimal Fee { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:n2}")]
+        public decimal RequestPrice { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:n2}")]
+        public decimal FinalPrice { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:n8}")]
+        public decimal FinalQuantity { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:n2}")]
+        public decimal FinalAmount { get; set; }
+    }
+
+
 }
+
